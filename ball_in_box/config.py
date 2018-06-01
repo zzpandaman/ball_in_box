93d05ed1ed3e8b37a48401a03629f40ebba251ca
zzpandaman <38775011+zzpandaman@users.noreply.github.com>
2018-06-01 10:29:17 +0800

    Add files via upload

---
+++ ball_in_box/config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# precision
+PERCISION = 500
+# the number of ballons
+NUM_OF_CIRCLE = 5
+# the number of blockers
+NUM_OF_BLOCKERS = 2
+# the range of X-axis and Y-axis
+XRANGE = (-1, 1)
+YRANGE = (-1, 1)
